cf96311e217afbc5756ea78ecfb5ee4a9afc1b2a
Alexey Kostyuchik <akostyuchik@nextype.ru>
2020-09-08 10:14:49 +0500

    начал делать главную

--- src/footer.php
+++ src/footer.php
@@ -1,12 +1,14 @@
 
 
-    <script src="./vendor/jquery/jquery.min.js"></script>
-    <script src="./vendor/bootstrap/bootstrap.min.js"></script>
-    <script src="./vendor/bootstrap/bootstrap-select.min.js"></script>
-    <script src="./vendor/jquery/jquery.modal.js"></script>
-    <script src="./vendor/jquery/jquery.validate.min.js"></script>
-    <script src="./vendor/maskedinput/jquery.maskedinput.min.js"></script>
-    <script src="./vendor/scrollbar/jquery.scrollbar.min.js"></script>
-    <script src="./script.js"></script>
+    </div>
+
+    <script src="../vendor/jquery/jquery.min.js"></script>
+    <script src="../vendor/bootstrap/bootstrap.min.js"></script>
+    <script src="../vendor/bootstrap/bootstrap-select.min.js"></script>
+    <script src="../vendor/jquery/jquery.modal.js"></script>
+    <script src="../vendor/jquery/jquery.validate.min.js"></script>
+    <script src="../vendor/maskedinput/jquery.maskedinput.min.js"></script>
+    <script src="../vendor/scrollbar/jquery.scrollbar.min.js"></script>
+    <script src="../script.js"></script>
 </body>
 </html>
---
+++ src/html/main-page.php
@@ -0,0 +1,24 @@
+<? include '../header.php' ?>
+
+
+<div class="main-page">
+    <div class="wrapper-section">
+        
+            <div class="services">
+                <div class="container">
+                    <h1 class="main-title">Мы делаем бизнес клиентов <span>сильнее</span></h1>
+                    <div class="button">
+                        <a href="javascript:void(0)" class="btn btn-primary">Отправить заявку</a>
+                    </div>
+                </div>
+
+            </div>
+        
+
+    </div>
+</div>
+
+
+
+
+<? include '../footer.php' ?>
