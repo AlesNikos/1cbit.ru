f52171262fcfd24fef02daad0214e35f8f01820b
Pavel Ryumin <pryumin@nextype.ru>
2020-10-13 10:19:49 +0500

    правки

--- src/html/article-portfolio/article-portfolio.php
+++ src/html/article-portfolio/article-portfolio.php
@@ -231,6 +231,47 @@
                                 <a href="#">Смотреть документ отзыва</a>
                             </div>
                         </div>
+                        <div class="projects-section">
+                            <div class="section-name">
+                                <h2 class="title">Смотрите другие проекты</h2>
+                                <a href="#" class="all-link">Все проекты</a>
+                            </div>
+                            <div class="items projects-items">
+                                <div class="item-wrap">
+                                    <a href="#" class="item">
+                                        <div class="img">
+                                            <img src="../../img/projects/image-1.png" alt="" class="grayscale">
+                                        </div>
+                                        <div class="text">Автоматизация налогового учёта в японской корпорации
+                                            Yamaha</div>
+                                    </a>
+                                </div>
+                                <div class="item-wrap">
+                                    <a href="#" class="item">
+                                        <div class="img">
+                                            <img src="../../img/projects/image-2.png" alt="" class="grayscale">
+                                        </div>
+                                        <div class="text">Внедрение «1С:ERP» в глобальной табачной компании, ведущей
+                                            в Южной Корее и пятой в мире
+                                            <i class="icon icon-play"></i>
+                                        </div>
+                                    </a>
+                                </div>
+                                <div class="item-wrap">
+                                    <a href="#" class="item">
+                                        <div class="img">
+                                            <img class="grayscale" src="../../img/projects/image-3.png" alt="">
+                                        </div>
+                                        <div class="text">Внедрение «1С:ERP» на крупнейшем производстве ювелирных
+                                            изделий из
+                                            золота и серебра</div>
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="button-wrap">
+                                <a href="#" class="btn-more">Все проекты</a>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
